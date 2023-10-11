d66d378a44df323bdbf65400e5a732dd324783c8
demitryedwards <dxyz773@gmail.com>
2023-10-10 23:29:20 -0400

    add tailwindcss and prettier.config and begin styling

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,12 +9,14 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto] ">
       {isLoading && <Loading />}
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-auto">
+        <main className="mx-auto max-w-3xl ">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
