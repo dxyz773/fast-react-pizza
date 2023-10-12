dc56835541313dbe5dd9ca9d67bdc050dedee7ed
demitryedwards <dxyz773@gmail.com>
2023-10-11 22:43:13 -0400

    Add dynamic Button component, LinkButton component, and update Loading component

---
+++ src/ui/LinkButton.jsx
@@ -0,0 +1,19 @@
+import { Link, useNavigate } from "react-router-dom";
+
+function LinkButton({ children, to }) {
+  const navigate = useNavigate();
+  const classname = "text-sm text-blue-500 hover:text-blue-600 hover:underline";
+  if (to === "-1")
+    return (
+      <button className={classname} to={to} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
+  return (
+    <Link className={classname} to={to}>
+      {children}
+    </Link>
+  );
+}
+
+export default LinkButton;
