51350ddd429d00907c394febc8a8e364923532a8
demitryedwards <dxyz773@gmail.com>
2023-10-26 14:47:06 -0400

    Add DeleteItem component

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,13 @@
-const { createSlice } = require("@reduxjs/toolkit");
+import { createSlice } from "@reduxjs/toolkit";
 
-const pizza12 = {
-  pizzaId: 12,
-  name: "Mediterranean",
-  quantity: 2,
-  unitPrice: 16,
-  totalPrice: 32,
-};
+// const pizza12 = {
+//   pizzaId: 12,
+//   name: "Mediterranean",
+//   quantity: 2,
+//   unitPrice: 16,
+//   totalPrice: 32,
+// };
+
 const initialState = { cart: [] };
 
 const cartSlice = createSlice({
@@ -22,6 +23,7 @@ const cartSlice = createSlice({
       state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
     },
     increaseItemQuantity(state, action) {
+      //payload = pizzaId
       const selected = state.cart.find(
         (item) => item.pizzaId === action.payload,
       );
@@ -29,6 +31,7 @@ const cartSlice = createSlice({
       selected.totalPrice = selected.unitPrice * selected.quantity;
     },
     decreaseItemQuantity(state, action) {
+      //payload = pizzaId
       const selected = state.cart.find(
         (item) => item.pizzaId === action.payload,
       );
@@ -50,3 +53,16 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((acc, curr) => acc + curr.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((acc, curr) => acc + curr.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
+// Look into 'reselect' library for using selector functions in large applications as it might cause performance issues in large applications. This library can help with optimization
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-function Button({ children, disabled = false, to, type }) {
+function Button({ children, disabled = false, to, type, onClick }) {
   const base =
     "inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
   const styles = {
@@ -13,6 +13,12 @@ function Button({ children, disabled = false, to, type }) {
       <Link className={styles[type]} to={to}>
         {children}
       </Link>
+    );
+  if (onClick)
+    return (
+      <button className={styles[type]} disabled={disabled} onClick={onClick}>
+        {children}
+      </button>
     );
   return (
     <button className={styles[type]} disabled={disabled}>
